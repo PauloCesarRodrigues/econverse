704b883532220f224b9a695c52f22d149ed09cd6
Paulo Cesar Rodrigues <pauloceasrrtv321@gmail.com>
2025-03-18 12:56:15 -0300

    fix: next image domains

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,13 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts'],
   images: {
-    domains: ['app.econverse.com.br', 'github.com'],
+    domains: [
+      'app.econverse.com.br',
+      'github.com',
+      'vercel.com',
+      '*.vercel.app',
+      '*-paulocesarrodrigues-projects.vercel.app',
+    ],
   },
   webpack(config, { isServer }) {
     if (!isServer) {
