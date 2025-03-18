22a21bdaf70439ee16be41e5168d4b3789dcaecb
Paulo Cesar Rodrigues <pauloceasrrtv321@gmail.com>
2025-03-18 13:24:19 -0300

    deploy vercel

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app'
-import '@/styles/carousel.sass'
 import { Header } from '@/components/header/index'
 import { Footer } from '@/components/footer/index'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '@/lib/react-query'
+
+import '@/styles/carousel.sass'
 import '@/styles/globals.sass'
+import '@/styles/_variables.sass'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
